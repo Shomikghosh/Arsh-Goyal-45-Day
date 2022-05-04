d738b5948f8f3386b9a70dfa8d9e2a79f43182f9
Shomik Ghosh <59383763+Shomikghosh@users.noreply.github.com>
2022-05-05 01:04:01 +0530

    Heaps-1

--- Heaps /1.Top K Frequent Elements.cpp
+++ Heaps /1.Top K Frequent Elements.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> topKFrequent(vector<int>& nums, int k){
         priority_queue <pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         unordered_map <int,int> mp;
         for(int i=0;i<nums.size();i++)
