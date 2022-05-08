34f6ca1136d01b6fb6505c3bbe4dfcfa6a5c7225
Shomik Ghosh <59383763+Shomikghosh@users.noreply.github.com>
2022-05-08 22:52:14 +0530

    Array-8

---
+++ Arrays/8.Two Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++)
+            v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        int i=0,j=nums.size()-1;
+        while(i<j)
+        {
+            if(v[i].first+v[j].first>target)
+                j--;
+            else if(v[i].first+v[j].first<target)
+                i++;
+            else
+                return {v[i].second,v[j].second};
+        }
+        return {-1,-1};
+    }
+};
